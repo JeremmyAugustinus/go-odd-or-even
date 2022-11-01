e8a7e70302d4abb655e3771df74bae6ea2b282a1
Jeremmy Augustinus <jeremmy.augustinus@emveep.com>
2022-11-01 15:14:52 +0700

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JeremmyAugustinus/go-odd-or-even
+
+go 1.19
---
+++ oddOrEven.go
@@ -0,0 +1,9 @@
+package go_odd_or_even
+
+func OddOrEven(i int) string {
+	if i%2 == 0 {
+		return "even"
+	} else {
+		return "odd"
+	}
+}
